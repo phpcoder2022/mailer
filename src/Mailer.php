c5a5feca879e95610704f5a7cd24f79614fe6672
phpcoder2022 <phpcoder2022@yandex.ru>
2023-08-25 18:43:30 +0300

    Mailer: Адреса и тема письма вынесены в конфиг

--- src/Mailer.php
+++ src/Mailer.php
@@ -6,6 +6,7 @@ namespace Phpcoder2022\SimpleMailer;
  * @psalm-type ErrorMessage = array{fieldName: string, message: string}
  * @psalm-type ResultMessage = array{result: bool, message: string}
  * @psalm-type FormatFormDataResult = array{mode: 'mail', message: string} | array{mode: 'error', messages: non-empty-list<ErrorMessage>}
+ * @psalm-type MailAddressesData = array{To: string, Subject: string, From: string, Reply-To: string}
  */
 final class Mailer
 {
@@ -57,17 +58,18 @@ final class Mailer
         if (self::isLocalhost()) {
             $result = false;
         } else {
+            $mailAddressesData = self::getMailAddressesAndSubject();
             $result = mail(
-                'w-wave.radio@yandex.ru',
-                'Интеграция: новый клиент',
+                $mailAddressesData['To'],
+                $mailAddressesData['Subject'],
                 preg_replace(
                     ['/></u', '/\s+/u', '/([^\s;]{50}|;)(\S)/u'],
                     [">\r\n<", "\r\n", "\\1<span></span\r\n>\\2"],
                     $text
                 ) . "\r\n",
                 [
-                    'From' => 'letter-collector@w-wave.nebosvod-msk.ru',
-                    'Reply-To' => 'phpcoder2022@yandex.ru',
+                    'From' => $mailAddressesData['From'],
+                    'Reply-To' => $mailAddressesData['Reply-To'],
                     'Content-Type' => 'text/html; charset=utf-8',
                     'X-Mailer' => 'PHP/' . phpversion(),
                 ],
@@ -83,5 +85,29 @@ final class Mailer
             || !($_SERVER['SERVER_ADDR'] ?? '')
             || $_SERVER['SERVER_NAME'] === 'localhost'
             || $_SERVER['SERVER_ADDR'] === '127.0.0.1';
+    }
+
+    /**
+     * @return MailAddressesData
+     */
+    private static function getMailAddressesAndSubject(): array
+    {
+        $addressesFileName = 'mails.txt';
+        $lineArr = is_file($addressesFileName)
+            ? file($addressesFileName, FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES)
+            : [];
+        $resultArr = ['To' => '', 'Subject' => '', 'From' => '', 'Reply-To' => ''];
+        foreach ($resultArr as $entryKey => $_) {
+            for ($i = 0; $i < count($lineArr); $i++) {
+                if (preg_match('/^\s*#\s*' . $entryKey . ':\s*(.*?)\s*/iu', $lineArr[$i])) {
+                    $resultArr[$entryKey] = trim($lineArr[$i + 1] ?? '');
+                    break;
+                }
+            }
+            if (!$resultArr[$entryKey]) {
+                throw new \UnexpectedValueException("Ключ $entryKey не определён в файле $addressesFileName");
+            }
+        }
+        return $resultArr;
     }
 }
