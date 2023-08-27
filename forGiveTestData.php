a90de50f2c7255fdbf63ace2b978a5ed86304861
phpcoder2022 <phpcoder2022@yandex.ru>
2023-08-27 12:21:28 +0300

    Исправление forGiveTestData.php под новые классы

--- forGiveTestData.php
+++ forGiveTestData.php
@@ -1,6 +1,7 @@
 <?php
 
-use Phpcoder2022\SimpleMailer\Mailer;
+use Phpcoder2022\SimpleMailer\Formatter;
+use Phpcoder2022\SimpleMailer\AboutFormLandingFieldsData;
 
 require_once './vendor/autoload.php';
 
@@ -13,7 +14,7 @@ foreach ($input as ['method' => $method, 'args' => $args]) {
     $output[] = [
         'method' => $method,
         'args' => $args,
-        'output' => (new \ReflectionClass(Mailer::class))->getMethod($method)->invoke(null, ...$args),
+        'output' => (new Formatter(AboutFormLandingFieldsData::createWithData(), $args[0]))->format(),
     ];
 }
 file_put_contents('testOutput.json', json_encode($output, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
