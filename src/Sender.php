bcacf4bd2ccd4465f9288104df550fc0f1e2d1cc
phpcoder2022 <phpcoder2022@yandex.ru>
2023-08-27 16:43:55 +0300

    Добавлено понятное исключение, если результата не существует

--- src/Sender.php
+++ src/Sender.php
@@ -12,6 +12,7 @@ final class Sender
 
     private readonly Formatter $formatter;
     private readonly Logger $logger;
+    private bool $isRan = false;
     private bool $lastOperationResult = false;
     private bool $lastFormComplete = false;
     private string $title = '';
@@ -26,6 +27,7 @@ final class Sender
 
     public function sendForm(array $formData): void
     {
+        $this->isRan = true;
         $formatResult = $this->formatter->format($formData);
         $this->lastFormComplete = $formatResult['mode'] === 'mail';
         $mailMessage = null;
@@ -47,22 +49,36 @@ final class Sender
 
     public function getLastOperationResult(): bool
     {
+        $this->throwIfNotRan();
         return $this->lastOperationResult;
     }
 
     public function getLastFormComplete(): bool
     {
+        $this->throwIfNotRan();
         return $this->lastFormComplete;
     }
 
     public function getResultAsJson(): string
     {
+        $this->throwIfNotRan();
         return json_encode(['header' => $this->header, 'textItems' => $this->textItems], JSON_UNESCAPED_UNICODE);
     }
 
     public function getResultAsHtml(): string
     {
+        $this->throwIfNotRan();
         $messageItems = array_map(fn ($subArr) => ['message' => $subArr['message']], $this->textItems);
         return HtmlViewer::loadTemplate($this->title, $this->header, $messageItems, $this->lastFormComplete);
+    }
+
+    private function throwIfNotRan(): void
+    {
+        $class = self::class;
+        if (!$this->isRan) {
+            throw new \LogicException(
+                "Отправка формы ни разу не запускалась с этого объекта $class. Результат не существует."
+            );
+        }
     }
 }
