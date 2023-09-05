a115b9619c8218eaba7a25a5df0e97b64266463b
phpcoder2022 <phpcoder2022@yandex.ru>
2023-09-05 17:25:01 +0300

    LogFormatter: включена поддержка наследования

--- src/Log/LogFormatter.php
+++ src/Log/LogFormatter.php
@@ -7,10 +7,10 @@ use Psr\Log\LogLevel;
 
 class LogFormatter
 {
-    private const DEFAULT_TITLE_LENGTH = 36;
-    private const TITLE_TAIL_LENGTH = 3;
+    protected const DEFAULT_TITLE_LENGTH = 36;
+    protected const TITLE_TAIL_LENGTH = 3;
 
-    public function __construct(private readonly LoggerInterface $logger)
+    public function __construct(protected readonly LoggerInterface $logger)
     {
     }
 
@@ -18,19 +18,19 @@ class LogFormatter
     {
         $this->logger->log(
             $level,
-            self::getSeparator(date('d.m.Y H:i:s') . ' ' . $level) . PHP_EOL
+            static::getSeparator(date('d.m.Y H:i:s') . ' ' . $level) . PHP_EOL
                 . json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT) . PHP_EOL
-                . self::getSeparator() . PHP_EOL
+                . static::getSeparator() . PHP_EOL
                 . PHP_EOL
         );
     }
 
-    private static function getSeparator(string $title = ''): string
+    protected static function getSeparator(string $title = ''): string
     {
         $titleLen = mb_strlen($title);
         $halfTitleLen = intval($titleLen / 2);
-        $maxLen = $titleLen <= self::DEFAULT_TITLE_LENGTH - self::TITLE_TAIL_LENGTH * 2
-            ? self::DEFAULT_TITLE_LENGTH
+        $maxLen = $titleLen <= static::DEFAULT_TITLE_LENGTH - static::TITLE_TAIL_LENGTH * 2
+            ? static::DEFAULT_TITLE_LENGTH
             : $titleLen + $titleLen % 2 + self::TITLE_TAIL_LENGTH * 2;
         $halfMaxLen = intval($maxLen / 2);
         return str_repeat('-', $halfMaxLen - ($halfTitleLen + $titleLen % 2))
